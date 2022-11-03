edd5444cafe3cea43cba28eb28c90571d629d9b3
Ryan Cronin <81655542+ryanjcronin5@users.noreply.github.com>
2022-11-03 17:17:15 +1300

    Add files via upload

---
+++ public/footer.php
@@ -0,0 +1,7 @@
+        <!--<footer>
+            <p>- Contact -</p>
+            <p>Developer: ryanjcronin5@gmail.com</p>
+            <p>Hosting Platform: <a href="https://www.apachefriends.org/about.html">Apache Friends</a></p>
+        </footer>-->
+    </body>
+</html>
